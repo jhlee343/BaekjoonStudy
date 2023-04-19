0bfef6b914b282ef2cf3533bcad284b92d3c1da9
reenact_l <77737814+jhlee343@users.noreply.github.com>
2023-04-19 15:20:17 +0900

    [Bronze III] Title: 최댓값, Time: 204 ms, Memory: 17680 KB -BaekjoonHub

---
+++ 백준/Bronze/2562. 최댓값/최댓값.java
@@ -0,0 +1,21 @@
+
+import java.util.*;
+public class Main{
+public static void main(String[] args) {
+	Scanner sc= new Scanner(System.in);
+	int max = 0;
+	int count=0;
+	int arr[] = new int[9];
+	for(int i =0 ; i<9;i++) {
+		arr[i]=sc.nextInt();
+		if(arr[i]>max) {
+			max=arr[i];
+			count = i+1;
+		}
+	}
+	System.out.println(max);
+	System.out.println(count);
+	sc.close();
+	}
+	
+}
