6333727772bd3ed94be6fa1ef836ada9307899ab
reenact_l <77737814+jhlee343@users.noreply.github.com>
2023-06-10 15:50:06 +0900

    [Bronze V] Title: 아스키 코드, Time: 216 ms, Memory: 17684 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -0,0 +1,13 @@
+
+import java.util.*;
+import java.lang.*;
+
+public class Main{
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		char c = sc.next().charAt(0);
+		int num = (int)c;
+		System.out.println(num);
+		sc.close();
+	}
+}
