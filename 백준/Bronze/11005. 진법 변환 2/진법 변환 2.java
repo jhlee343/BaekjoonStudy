f3ff47c3bf3a821b0cef81577ca25b94f8dac953
reenact_l <77737814+jhlee343@users.noreply.github.com>
2023-07-25 23:25:22 +0900

    [Bronze I] Title: 진법 변환 2, Time: 208 ms, Memory: 17756 KB -BaekjoonHub

---
+++ 백준/Bronze/11005. 진법 변환 2/진법 변환 2.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Main{
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		int B = sc.nextInt();
+		
+		System.out.println(Integer.toString(N, B).toUpperCase());
+		sc.close();
+	}
+}
