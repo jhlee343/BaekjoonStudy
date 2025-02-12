288edc0e6e6da0e6aaabac864c98b2a5dd5292fd
reenact_l <77737814+jhlee343@users.noreply.github.com>
2025-02-12 11:49:42 +0900

    [Bronze V] Title: 이상한 기호, Time: 96 ms, Memory: 12864 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15964. 이상한 기호/이상한 기호.java
+++ 백준/Bronze/15964. 이상한 기호/이상한 기호.java
@@ -4,10 +4,10 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner sc = new Scanner(System.in);
-		int a = sc.nextInt();
-		int b = sc.nextInt();
+		long a = sc.nextInt();
+		long b = sc.nextInt();
 		
-		int answer = (a+b)*(a-b);
+		long answer = (a+b)*(a-b);
 		System.out.println(answer);
 		
 		sc.close();
