544f09a55859e4b307fc5f93daba0512c1a0a5ca
reenact_l <77737814+jhlee343@users.noreply.github.com>
2025-03-03 12:22:21 +0900

    [Silver IV] Title: 청기 백기, Time: 100 ms, Memory: 14536 KB -BaekjoonHub

---
+++ 백준/Silver/15736. 청기 백기/청기 백기.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		double n = sc.nextDouble();
+		
+		System.out.println((int)Math.sqrt(n));
+		
+	}
+
+}
